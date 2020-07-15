f98ed3e2bc6b10ef47627c1432f60087242be348
ashrithaK <ak2529@njit.edu>
2020-07-15 16:03:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main { //example to implement HashMap
+  public static void main(String[] args) {
+    String str="This is is is a a a a sample sample stmt";
+    HashMap map=new HashMap<String, Integer>();
+    String subStr[]= str.split(" ");
+    //int count=1;
+    for(int i=0;i<subStr.length;i++){
+      if(map.containsKey(subStr[i])){
+        int count=(int)map.get(subStr[i]);
+        map.put(subStr[i],count+1);
+      }
+      else
+      map.put(subStr[i],1);      
+    }
+    System.out.println(map);
+  }
+}
